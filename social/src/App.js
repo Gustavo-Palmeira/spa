a54537faeac89d81d9faf363c3e374a783ef1b68
GUSTAVO PALMEIRA PINTO <gustavo.ppinto@senacsp.edu.br>
2022-03-03 21:29:27 -0300

    Social

---
+++ social/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import Header from "./components/Header"
+import NewPost from "./components/NewPost"
+import Feed from "./components/Feed"
+
+const App = () => {
+    const [username, setUsername] = useState("Gustavo Palmeira")
+    const [posts, setPosts] = useState([])
+
+    console.log(posts)
+
+    return (
+        <>
+            <Header username={username} setUsername={setUsername} />
+            <NewPost posts={posts} setPosts={setPosts} username={username} />
+            <Feed posts={posts} />
+        </>
+    )
+}
+
+export default App
